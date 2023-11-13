7473eefa96ce09964a17e2b03188841beced4971
fathimabida <fathimahabidah@student.telkomuniversity.ac.id>
2023-11-13 11:36:17 +0700

    MODUL3 FATHIMAH

---
+++ MODUL3 FATHIMAH/JURNAL MODUL 3/create.php
@@ -0,0 +1,48 @@
+<!-- Pada file ini kalian membuat coding untuk logika create / menambahkan mobil pada showroom -->
+
+<?php
+// (1) Jangan lupa sertakan koneksi database dari yang sudah kalian buat yaa
+
+include("connect.php");
+
+// (2) Buatlah perkondisian untuk memeriksa apakah permintaan saat ini menggunakan metode POST
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+// (3) Jika sudah coba deh kalian ambil data dari form (CLUE : pakai POST)
+
+    // a. Ambil data nama mobil
+
+    // b. Ambil data brand mobil
+
+    // c. Ambil data warna mobil
+
+    // d. Ambil data tipe mobil
+
+    // e. Ambil data harga mobil
+    $namaMobil = $_POST['nama_mobil'];
+    $brandMobil = $_POST['brand_mobil'];
+    $warnaMobil = $_POST['warna_mobil'];
+    $tipeMobil = $_POST['tipe_mobil'];
+    $hargaMobil = $_POST['harga_mobil']; 
+
+} 
+    
+    // (4) Kalau sudah, kita lanjut Query / Menambahkan data pada SQL (Disini ada perintah untuk SQL), Masukkan ke tabel showroom_mobil (include setiap nama column)
+    $query = mysqli_query($connect, "INSERT INTO showroom_mobil(nama_mobil, brand_mobil, warna_mobil, tipe_mobil, harga_mobil) VALUES ('$namaMobil', '$brandMobil', '$warnaMobil', '$tipeMobil', '$hargaMobil')");
+
+    // (5) Buatkan kondisi jika eksekusi query berhasil
+    if ($query) {
+        echo "<script>alert('Data mobil diterima')</script>";
+        echo "<meta http-equiv='refresh' content'1 url=form_create_mobil.php'>";
+    }
+
+    // (6) Jika terdapat kesalahan, buatkan eksekusi query gagalnya 
+
+    else {
+        echo "<script>alert('Data Mobil Tidak Berhasil Ditambahkan')</script>";
+        echo "<meta http-equiv='refresh' content'1 url=form_create_mobil.php'>";
+    }
+
+// (7) Tutup koneksi ke database setelah selesai menggunakan database
+?>
---
+++ MODUL3 FATHIMAH/JURNAL MODUL 3/list_mobil.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>List Mobil</title>
+</head>
+<body>
+    <?php include("navbar.php") ?>
+    <center>
+        <div class="container">
+            <table class="table">
+            <h1>List Mobil</h1>
+
+            <thead>
+                <tr> 
+                    <th scope="col">Nama Mobil</th>;
+                    <th scope="col">Brand Mobil</th>;
+                    <th scope="col">Warna Mobil</th>;
+                    <th scope="col">Tipe Mobil</th>;
+                    <th scope="col">Harga Mobil</th>;
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+            include("connect.php");
+
+            // Buatlah query untuk mengambil data dari database (gunakan query SELECT)            
+            $query = mysqli_query($connect, "SELECT * FROM showroom_mobil");
+
+            // Buatlah perkondisian dimana: 
+            // 1. a. Apabila ada data dalam database, maka outputnya adalah semua data dalam database akan ditampilkan 
+            //       (gunakan num_rows > 0, while, dan mysqli_fetch_assoc())
+            //    b. Untuk setiap data yang ditampilkan, buatlah sebuah button atau link yang akan mengarahkan web ke halaman 
+            //       form_detail_mobil.php dimana halaman tersebut akan menunjukkan seluruh data dari satu mobil berdasarkan id
+            // 2. Apabila tidak ada data dalam database, maka outputnya adalah pesan 'tidak ada data dalam tabel'
+
+            //<!--  **********************  1  **************************     -->
+            $num_rows = mysqli_num_rows($query);
+
+            if ($num_rows > 0){
+                while ($selects = mysqli_fetch_assoc($query)) {
+                 
+            ?>
+
+            <tr>
+                    <th scope="row"><?= $selects['id']?></th>
+                    <td><?= $selects['nama_mobil']?></td>
+                    <td><?= $selects['brand_mobil']?></td>
+                    <td><?= $selects['warna_mobil']?></td>
+                    <td><?= $selects['tipe_mobil']?></td>
+                    <td><?= $selects['harga_mobil']?></td>
+                    <td><input type="submit" class="btn btn-primary" action="form_detail_mobil.php" value="Detail Mobil"></td>
+            </tr>
+            <?php
+
+                }
+            }
+    
+            
+            //<!--  **********************  1  **************************     -->
+
+            //<!--  **********************  2  **************************     -->
+            else {
+                "<script>alert('Tidak Ada Data Dalam Tabel!')</script>";
+
+            }
+
+            //<!--  **********************  2  **************************     -->
+            ?>
+        </tbody>
+        </table>
+        </div>
+    </center>
+</body>
+</html>
